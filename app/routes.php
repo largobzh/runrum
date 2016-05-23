5144adafe9a81b2191dc8b1dda6f653ed7064c2b
gyojishukke <thierry@mezenge.fr>
2016-05-23 16:20:07 +0200

    sauvegarde

--- app/routes.php
+++ app/routes.php
@@ -2,4 +2,15 @@
 	
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'home'],
+
+		// route pour la creation des carnets
+		['GET|POST','/creationcarnet','Carnet#creationCarnet','creationCarnet'],
+		// route pour afficher la liste des carnets
+		['GET|POST','/listecarnet','Carnet#afficherCarnet','afficherCarnet'],
+		// route pour la modification une note du carnet
+		['GET|POST','/modifiercarnet/[i:id]','Carnet#modifierCarnet','modifierCarnet'],
+		// afin de supprimer une note du carnet
+		['GET|POST','/listecarnet/[i:id]','Carnet#supprimerCarnet','supprimerCarnet'],
+
+
 	);
